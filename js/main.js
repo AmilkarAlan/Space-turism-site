842b084990ab7604733cf83f634ff33a5dea03f6
Amilkar Sanchez <amilkaralan@gmail.com>
2025-01-21 20:09:56 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const headerPlaceholder = document.getElementById('header-placeholder');
+
+fetch('./header.html')
+.then(response => {
+    if (!response.ok) {
+        throw new Error(`!Error HTTP con status: ${response.status}`);
+    }
+
+    return response.text();  
+})
+.then(data => {
+    console.log(data);
+    
+    headerPlaceholder.innerHTML = data;
+})
+.catch(error => {
+    console.error('Ocurrió un error!, error:', error);
+});
